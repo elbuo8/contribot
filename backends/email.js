667d1d76b313c857f1a80c279a8941acb9df0dbe
elbuo8 <yamil.asusta@sendgrid.com>
2014-02-15 22:34:01 +0400

    debug

--- backends/email.js
+++ backends/email.js
@@ -2,6 +2,7 @@
 var sendgrid = require('sendgrid')(process.env.SENDGRID_USER, process.env.SENDGRID_PWD);
 
 exports.dispatch = function (userinfo) {
+  console.log(userinfo);
   sendgrid.send({
     to: process.env.EMAIL_TO,
     from: process.env.EMAIL_FROM,
