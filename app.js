9954ee96ebb8b7361003c6ca25d17883b2e55173
elbuo8 <yamil.asusta@sendgrid.com>
2014-02-15 18:17:36 +0400

    error on path location syntax

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + 'views');
+app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.cookieParser());
