f03e67823810c3324e59cc578d547c8158f09e10
elbuo8 <yamil.asusta@sendgrid.com>
2014-05-09 16:26:28 -0400

    DB rename, Auth function, better authentication flow

--- contribot/utils.go
+++ contribot/utils.go
@@ -2,14 +2,16 @@ package contribot
 
 import (
 	"github.com/go-martini/martini"
+	"github.com/martini-contrib/sessions"
 	"labix.org/v2/mgo"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 )
 
 func MapServices(m *martini.ClassicMartini) {
-	session, err := mgo.Dial(os.Getenv("DB_URL"))
+	db, err := mgo.Dial(os.Getenv("DB_URL"))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -18,16 +20,23 @@ func MapServices(m *martini.ClassicMartini) {
 	go func() {
 		for sig := range c {
 			log.Println("%v captured - Closing database connection", sig)
-			session.Close()
+			db.Close()
 			os.Exit(1)
 		}
 	}()
 
-	m.Map(session)
+	m.Map(db)
+}
+
+func Gandalf(req *http.Request, res http.ResponseWriter, session sessions.Session) {
+	if session.Get("user") == "" {
+		http.Redirect(res, req, "/auth", http.StatusOK)
+	}
 }
 
 func MapRoutes(m *martini.ClassicMartini) {
 	m.Post("/githook", HandleGitHook)
 	m.Get("/auth", AuthGitHub)
-	m.Get("/award", GitHubAuthMiddleware, AwardUser)
+	m.Get("/githubAuth", GitHubAuthMiddleware, GetUserFromToken)
+	m.Get("/award", Gandalf, AwardUser)
 }
