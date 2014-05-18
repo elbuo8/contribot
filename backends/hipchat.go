86849a5b3f0eaa4b44c7b9cb6b3bfaa811174229
Brandon West <brawest@gmail.com>
2014-05-18 08:00:33 -0600

    whitespace

--- backends/hipchat.go
+++ backends/hipchat.go
@@ -9,9 +9,9 @@ import (
 )
 
 type HipChatOptions struct {
-	Token  string
-	Room   string
-	Color  string
+	Token string
+	Room  string
+	Color string
 }
 
 func HipChat(opts *HipChatOptions) contribot.Backend {
@@ -24,8 +24,8 @@ func sendNotification(opts *HipChatOptions, sub *contribot.Submission) {
 	urlStr := fmt.Sprintf("https://api.hipchat.com/v2/room/%s/notification?auth_token=%s", opts.Room, opts.Token)
 	blob := make(map[string]string)
 	blob["color"] = opts.Color
-  blob["message_format"] = "html"
-  blob["message"] = fmt.Sprintf("New Contributor!<br/><strong>Name:</strong> %s<br/><strong>Address:</strong> %s<br/><strong>Email:</strong> %s<br/><strong>Size:</strong> %s", sub.Name, sub.Address, sub.Email, sub.Size)
+	blob["message_format"] = "html"
+	blob["message"] = fmt.Sprintf("New Contributor!<br/><strong>Name:</strong> %s<br/><strong>Address:</strong> %s<br/><strong>Email:</strong> %s<br/><strong>Size:</strong> %s", sub.Name, sub.Address, sub.Email, sub.Size)
 	body, _ := json.Marshal(blob)
 	req, _ := http.NewRequest("POST", urlStr, bytes.NewReader(body))
 	req.Header.Set("User-Agent", "ContriBot")
